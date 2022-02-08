49e8c7f12db0cb3ba6bfc59ef803948c0fd40dc7
avbprogramming <avb.programming@gmail.com>
2022-02-08 12:40:35 +0300

    Feb8_Job_v3

--- test/allTest.java
+++ test/allTest.java
@@ -24,8 +24,8 @@ public class allTest {
 
     @Test
     public void DecimalToOctal() {
-            String str = DecimalToOctal.toDecimal("10");
-            Assert.assertEquals("8", str);
+        String str = DecimalToOctal.toDecimal("10");
+        Assert.assertEquals("8", str);
     }
 
     @Test
@@ -35,13 +35,13 @@ public class allTest {
     }
 
     @Test
-    public void HexToDecimal(){
+    public void HexToDecimal() {
         String str = HexToDecimal.toDecimal("f");
         Assert.assertEquals("15", str);
     }
 
     @Test
-    public void OctalToDecimal(){
+    public void OctalToDecimal() {
         String str = OctalToDecimal.toOctal("10");
         Assert.assertEquals("12", str);
     }
@@ -53,41 +53,86 @@ public class allTest {
     }
 
     @Test
-    public void MathFunctionPlus(){
+    public void MathFunctionPlus() {
         String firstNum = "22";
         String secondNum = "11";
         MathFunction.plus(firstNum, secondNum);
-        Assert.assertEquals("2","11", "11");
+        Assert.assertEquals("2", "11", "11");
 
     }
 
     @Test
-    public void MathFunctionMinus(){
-         MathFunction.minus("22", "11");
-        Assert.assertEquals("2","11", "11");
-     }
+    public void MathFunctionMinus() {
+        MathFunction.minus("22", "11");
+        Assert.assertEquals("2", "11", "11");
+    }
 
-     @Test
-    public void MathFunctionMultiply(){
+    @Test
+    public void MathFunctionMultiply() {
         MathFunction.multiply("22", "11");
-        Assert.assertEquals("2","11", "11");
+        Assert.assertEquals("2", "11", "11");
         //         divide
     }
 
     @Test
-    public void MathFunctionDivide(){
+    public void MathFunctionDivide() {
         MathFunction.divide("22", "11");
-        Assert.assertEquals("2","11", "11");
+        Assert.assertEquals("2", "11", "11");
 
     }
 
     @Test
-    public void choiceTest(){
+    public void choiceTest() {
         Choice choice = new Choice();
-            Choice.choice(1, "22", "11");
-            Assert.assertEquals("Ответ: 33" , choice);
+        Choice.choice(1, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(2, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(3, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(4, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(5, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(6, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(7, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(8, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(9, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(10, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(11, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(12, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice.choice(13, "22", "11");
+        Assert.assertNotEquals("33", choice);
+
+        Choice choice1 = new Choice();
+        Choice.choice2(2, "1");
+        Assert.assertNotEquals("22", choice1);
     }
 
+    @Test
+    public void Menu(){
+        int num = Menu.begin();
+        Assert.assertNotEquals("22", num);
+    }
 
 
 }
